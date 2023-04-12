23f8a1de13caa9864576641bf3d6a03d1bdfc0ac
kalucky0 <kalucki23@gmail.com>
2023-04-12 22:29:54 +0200

    Create simple TCP client

--- lib/src/bqsp_client.dart
+++ lib/src/bqsp_client.dart
@@ -1,1 +1,39 @@
-class BqspClient {}
+import 'dart:async';
+import 'dart:io';
+
+class BqspClient {
+  late final Socket _socket;
+  late final _buffer = StringBuffer();
+  var _completer = Completer<void>();
+
+  Future connect(String host, int port) async {
+    _socket = await Socket.connect(host, port);
+    _socket.listen((data) {
+      _buffer.write(String.fromCharCodes(data));
+      if (_buffer.toString().endsWith('\x00')) {
+        _completer.complete();
+      }
+    });
+  }
+
+  Future<StringBuffer> sendAndWait(String message) async {
+    _socket.write(message);
+    await _completer.future;
+    _completer = Completer<void>();
+    return _buffer;
+  }
+
+  void send(String message) {
+    _socket.write(message);
+  }
+
+  Future<StringBuffer> receive() async {
+    await _completer.future;
+    _completer = Completer<void>();
+    return _buffer;
+  }
+
+  Future close() async {
+    _socket.destroy();
+  }
+}
--- test/bqsp_client_test.dart
+++ test/bqsp_client_test.dart
@@ -3,8 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bqsp_client/bqsp_client.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('create client, send and receive a message', () async {
     final client = BqspClient();
-    expect(client, isNotNull);
+    await client.connect('localhost', 8080);
+    final response = await client.sendAndWait('Hello, world!\x00');
+    expect(response.toString(), 'Hello, world!\x00');
+    await client.close();
+  });
+
+  test('create client, send a message, receive a message', () async {
+    final client = BqspClient();
+    await client.connect('localhost', 8080);
+    client.send('Hello, world!\x00');
+    final response = await client.receive();
+    expect(response.toString(), 'Hello, world!\x00');
+    await client.close();
   });
 }
